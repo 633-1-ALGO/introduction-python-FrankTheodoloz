20aa27aa2bc8c562260e23c8cc30f3d0dcdc5a5c
Frank Théodoloz <frank@16d.ch>
2019-10-06 19:51:06 +0200

    Exercices listes et tableaux 1 et 2 terminés

--- 12.1- Exercice - Listes et Tableaux/liste1.py
+++ 12.1- Exercice - Listes et Tableaux/liste1.py
@@ -5,4 +5,35 @@
 #                                             . .  .  .  .  .  .  .  .  .  .
 # Indication :   L'alignement rectiligne n'est pas une contrainte, tant que la table est visible ligne par ligne c'est ok.
 #               Si vous êtes perfectionnistes faites vous plaisir.
-liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 32]
+
+
+def valeur_max(_liste: list) -> int:
+    return len(str(liste[-1] * liste[-1]))
+
+
+def ajout_espace(_nombre: int) -> str:
+    buffer = ""
+    if _nombre == 0: _nombre = ""
+    for e in range(0, espace - len(str(_nombre))):
+        buffer += " "
+    buffer += str(_nombre)
+    return buffer
+
+
+def table_multiplication(_list: list):
+    ligne = ajout_espace(0)
+    for item in _list:
+        ligne += ajout_espace(item)
+    print(ligne)
+
+    for item_y in _list:
+        ligne = ajout_espace(item_y)
+        for item_x in _list:
+            result = item_x * item_y
+            ligne += ajout_espace(result)
+        print(ligne)
+
+
+espace = valeur_max(liste) + 1
+table_multiplication(liste)
--- 12.2- Exercice - Listes et Tableaux/liste2.py
+++ 12.2- Exercice - Listes et Tableaux/liste2.py
@@ -2,3 +2,25 @@
 # Résultat attendu : Un affichage comme ceci : "La valeur maximum est :  x  et elle se trouve à l'indice [ n ][ m ]
 
 tab = [[4, 7, 3, 20, 42], [2, 4, 5, 7, 2], [23, 24, 15, 75, 23]]
+
+
+def valeur_max(_tableau: list) -> None:
+    val_max = 0
+    max_x = 0
+    max_y = 0
+    x = 0
+    y = 0
+
+    for tab_y in _tableau:
+        for val_x in tab_y:
+            if val_x > val_max:
+                val_max = val_x
+                max_x = x
+                max_y = y
+                x += 1
+        y += 1
+    print(_tableau)
+    print("La valeur maximum est :", val_max, "et elle se trouve à l'indice [", max_y, "][", max_x, "] ([ ligne ][ valeur ])")
+
+
+valeur_max(tab)
