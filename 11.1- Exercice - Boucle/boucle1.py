f56964737e477ce84e98b8571f4c999be41e0849
Frank Théodoloz <frank@16d.ch>
2019-10-06 18:15:32 +0200

    Exercices boucle 1 et 2 terminés

--- 11.1- Exercice - Boucle/boucle1.py
+++ 11.1- Exercice - Boucle/boucle1.py
@@ -2,3 +2,17 @@
 # Données : un tableau A de n nombre réels
 # Résultat attendu : Moyenne des nombres réels du tableau A
 A = [1, 5, 15, 25, 10, 55, 50, 35]
+
+
+def moyenne_tableau(_list: list) -> float:
+    i = 0
+    somme = 0
+
+    while i < len(_list):
+        somme += _list[i]
+        i += 1
+
+    return somme / i
+
+
+print(moyenne_tableau(A))
--- 11.2- Exercice - Boucle/boucle2.py
+++ 11.2- Exercice - Boucle/boucle2.py
@@ -3,3 +3,20 @@
 # Résultat attendu : Le tableau B trié
 
 B = [2, 6, 8, 5, 4, 12, 98, 34, 1]
+
+
+def tri_insertion(_list: list) -> list:
+    i = 1  # indice départ (0 considéré comme classé)
+    while i < len(_list):
+        valeur = _list[i]
+        indice_test = i - 1  # indice précédent
+        while valeur < _list[indice_test] and indice_test >= 0:
+            _list[indice_test + 1] = _list[indice_test]  # déplace l'indice précédent vers la droite
+            _list[indice_test] = valeur
+            indice_test -= 1
+        i += 1
+
+    return _list
+
+
+print(tri_insertion(B))
