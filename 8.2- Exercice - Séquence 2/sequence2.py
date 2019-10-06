f4399ddcad0d934c8bcc85df4680e7d5f3235c57
Frank Théodoloz <frank@16d.ch>
2019-10-06 16:51:55 +0200

    Exercices séquence 1 et 2 terminés

--- 8.2- Exercice - Séquence 2/sequence2.py
+++ 8.2- Exercice - Séquence 2/sequence2.py
@@ -4,3 +4,9 @@
 
 nb_articles = 13
 prix_ht = 42.75
+
+print("Le prix TTC est de", nb_articles * prix_ht * 107.7 / 100, "chf")  # Le prix TTC est de 598.5427500000001 chf
+
+print("Le prix TTC est de",
+      "{:.2f}".format(round(nb_articles * prix_ht * 107.7 / 100 * 2, 1) / 2),
+      "chf")  # Le prix TTC est de 598.55 chf
